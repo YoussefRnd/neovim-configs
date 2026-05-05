663ba840603cb17f54864e4893ead1c64260dadb
Youssef Boumlaki <boumlaki2002@gmail.com>
2026-05-05 21:41:57 +0100

    chore: update bootsraping function

--- init.lua
+++ init.lua
@@ -9,14 +9,14 @@ require('autocmds')
 -- Install lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system({
+  vim.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
     lazypath,
-  })
+  }, { text = true }):wait()
 end
 vim.opt.rtp:prepend(lazypath)
 
